7084eb16ee482b45d3176359f602b5418d9a8025
havishketan <mastigaming927@gmail.com>
2026-08-02 21:15:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -323,3 +323,19 @@ camera.updateProjectionMatrix();
 renderer.setSize(window.innerWidth,window.innerHeight);
 
 });
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
